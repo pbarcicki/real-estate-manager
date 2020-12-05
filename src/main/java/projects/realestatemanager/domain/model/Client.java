b933d08dde6eabd5733ea08db172fdc53aeb63b6
unknown <justyna.kuznikk@gmail.com>
2020-12-05 15:40:32 +0100

    #3 skm :: user and client entity

--- src/main/java/projects/realestatemanager/domain/model/Client.java
+++ src/main/java/projects/realestatemanager/domain/model/Client.java
@@ -1,4 +1,39 @@
 package projects.realestatemanager.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "clients")
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+@EqualsAndHashCode
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, name = "client_name")
+    private String clientName;
+
+    @Column(nullable = false, name = "client_registration_date")
+    private LocalDate clientRegistrationDate;
+
+    @Column(nullable = false, name = "client_contact_number")
+    private String clientContactNumber;
+
+    @Column(nullable = false, name = "client_contact_email")
+    private String clientContactEmail;
+
+    @Column(nullable = false, name = "client_interest")
+    private String clientInterest;
+
+    @ManyToMany
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+
 }
--- src/main/java/projects/realestatemanager/domain/model/User.java
+++ src/main/java/projects/realestatemanager/domain/model/User.java
@@ -1,4 +1,41 @@
 package projects.realestatemanager.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="users")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@EqualsAndHashCode(of = "userName")
+@ToString(exclude = "userPassword")
 public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, name = "user_name")
+    private String userName;
+
+    @Column(nullable = false, name = "user_password")
+    private String userPassword;
+
+    @Column(nullable = false, name = "user_email")
+    private String userEmail;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    @ManyToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private Client client;
+
+
+
+
+
+
+
 }
