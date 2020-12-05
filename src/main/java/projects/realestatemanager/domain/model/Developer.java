4f2bb23b50499da013325fe8d427381e305339be
pbarcicki <pbarcicki@int.pl>
2020-12-05 15:40:06 +0100

    #3 Entities :: Developer entity added

--- src/main/java/projects/realestatemanager/domain/model/Developer.java
+++ src/main/java/projects/realestatemanager/domain/model/Developer.java
@@ -1,4 +1,24 @@
 package projects.realestatemanager.domain.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="developers")
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@EqualsAndHashCode(of = "developerName")
 public class Developer {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true) //todo zmienić na możliwość dodania kilku kontaktów dla developera
+    private String developerName;
+
+    @Column(nullable = false)
+    private String developerContactPerson;
+
+    @Column(nullable = false)
+    private String developerContactNumber;
 }
