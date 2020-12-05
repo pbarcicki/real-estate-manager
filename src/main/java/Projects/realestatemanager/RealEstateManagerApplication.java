1152928b5ef8fc450d70a8540a02aa705772d792
unknown <justyna.kuznikk@gmail.com>
2020-12-05 12:25:08 +0100

    #1 pkp :: initial project structure

---
+++ src/main/java/Projects/realestatemanager/RealEstateManagerApplication.java
@@ -0,0 +1,13 @@
+package Projects.realestatemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projects/realestatemanager/RealEstateManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package Projects.realestatemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
