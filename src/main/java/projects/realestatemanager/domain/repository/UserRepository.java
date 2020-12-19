a33e1a9607377a7360dc598cc9d27899e94d076e
OleksandrLysenko93 <tarabanovka3@gmail.com>
2020-12-19 10:32:48 +0100

    #43 html :: html optimization

--- src/main/java/projects/realestatemanager/domain/repository/UserRepository.java
+++ src/main/java/projects/realestatemanager/domain/repository/UserRepository.java
@@ -5,9 +5,12 @@ import projects.realestatemanager.domain.model.User;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     boolean existsByUserEmail(String userEmail);
+
+    Optional<User> findByUsername(String username);
 }
